5d1747837c7e018b1ea6da644a81540044acd96c
luka <salukvadze2008@gmail.com>
2025-03-12 16:32:27 +0400

    davalebaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'davaleba1';
+
+  imgsource = 'https://www.freeiconspng.com/uploads/backpack-icon-5.jpg'
+
+  firsttext = 'LEBRON MAX AIR'
+
+  secondtext = 'KIDS BACKPACK' 
+
+  thirdtext = '$65'
+
+  fourthtext = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rutrum risus sit amet porta fermentum. Duis vitae vulputate justo. Nullam semper, nibh quis condimentum rutrum, ipsum dui congue turpis, sed.'
+
+  buttontext = 'ADD TO CART'
 }
